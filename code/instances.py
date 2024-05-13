84a9fbdff184db032cefca453e0c4df7b2c75886
kazenoko-git <kazenoko.main@gmail.com>
2024-05-13 16:18:46 +0530

    Update

--- code/instances.py
+++ code/instances.py
@@ -1,6 +1,5 @@
 import subprocess, os, tracemalloc
 def libinstall(to_install:str):
-    subprocess.run('pip install --upgrade pip')
     sub = subprocess.run(f'pip install {to_install}')
     if sub.returncode == 1:
         print("ERROR: Check the module name or your internet connection.")
