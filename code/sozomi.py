25f357492da74198973b8c543fa603b14fa33575
kazenoko-git <kazenoko.main@gmail.com>
2024-05-13 15:14:43 +0530

    Update

--- code/sozomi.py
+++ code/sozomi.py
@@ -1,15 +1,23 @@
 import streamlit as st
-
-
+import ai
 st.set_page_config(page_title="Sozomi LLM", page_icon=":tada:", layout="wide") # https://www.webfx.com/tools/emoji-cheat-sheet/
-
 with st.container():
+    global AI
     st.header("sozomi LLM")
-    st.subheader("chat with Sozomi!")
+    st.text_input("your username: ", key="username")
+    name = str(st.session_state.username)
+    AI = ai.AI(username=name, model=1)
 
 with st.container():
-    st.text_input("chat with Sozomi!", key="prompt", label_visibility="hidden")
-    prompt = str(st.session_state.prompt)
-    AI = ai.AI(username="kazenoko", model=1)
-    result = AI.ask(prompt)
-    st.write(result)
+    if "username" not in st.session_state:
+        st.write("please enter your username first.")
+    else:
+        st.write("---")
+        st.subheader("chat with Sozomi!")
+        st.text_input("chat with Sozomi!", key="prompt", label_visibility="hidden")
+        if "prompt" not in st.session_state:
+            pass
+        else:
+            prompt = str(st.session_state.prompt)
+            result = AI.ask(prompt)
+            st.write(f"Sozomi: {result}")
